300cbe843bbda8de173e037cb674c7bf7f1c2556
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2022-07-07 17:28:00 +0900

    feat: Create DB, Test index.jsp, Test boardList.jsp as MVC
    DB
      create db, schema, table, Insert data.
    MVC
      create index.jsp, list.jsp
      create IndexController.java, BoardController.java
    application.yml
      add spring.mvc.view.prefix, suffix for use jsp
    build.gradle
      add jsp,jstl at build.gradle

---
+++ NOTICEBOARD/src/main/java/com/example/board/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.example.board.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController{
+	
+	@RequestMapping("/")
+	public ModelAndView indexPage() {
+		ModelAndView model = new ModelAndView("index");
+		LocalDateTime dt1 = LocalDateTime.now();
+		String dtStr = dt1.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+		model.addObject("serverTime", dtStr);
+		return model;
+	}
+}
