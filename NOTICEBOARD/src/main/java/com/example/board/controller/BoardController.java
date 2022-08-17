754f64d7ac009678b3a31a8230a71240ea27437e
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2022-08-17 17:57:05 +0900

    feat: add setting to use jsp and thymeleaf files
    prefer thymeleaf than jsp
    add a layout page in progress
    add a join page in progress
    fix board.sql

--- NOTICEBOARD/src/main/java/com/example/board/controller/BoardController.java
+++ NOTICEBOARD/src/main/java/com/example/board/controller/BoardController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,79 +23,89 @@ import com.example.board.service.BoardService;
 //import lombok.extern.slf4j.Slf4j;
 
 //@Slf4j
-@RequestMapping(value="/board/*")
+@RequestMapping(value = "/board/*")
 @Controller
-public class BoardController{
+public class BoardController {
 
-	@Autowired
-	private BoardService service;
+    @Autowired
+    private BoardService service;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Model model) throws Exception {
-		List<BoardDTO> list = service.list();
-		model.addAttribute("list", list);
-		
-		return "/board/list";
-	}
-	
-	@RequestMapping(value = "/regiView", method = RequestMethod.GET)
-	public String regiView() throws Exception{
-		return "/board/regi";
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/regi", method = RequestMethod.POST)
-	public String regi(BoardDTO dto) throws Exception {
-		Date date = new Date(System.currentTimeMillis());
-		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdHHmmss");
-		
-		dto.setReg_date(format.format(date));
-		if(service.regi(dto)==1) {
-			return "Y";
-		} else {
-			return "N";
-		}
-	}
-	
-	@RequestMapping(value="/view", method=RequestMethod.POST)
-	public String view(Model model, HttpServletRequest request) throws Exception{
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) throws Exception {
+        List<BoardDTO> list = service.list();
+        model.addAttribute("list", list);
+
+        return "/board/list";
+    }
+
+    @RequestMapping(value = "/regiView", method = RequestMethod.GET)
+    public String regiView() throws Exception {
+        return "/board/regi";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/regi", method = RequestMethod.POST)
+    public String regi(BoardDTO dto) throws Exception {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdHHmmss");
+
+        dto.setReg_date(format.format(date));
+        if (service.regi(dto) == 1) {
+            return "Y";
+        } else {
+            return "N";
+        }
+    }
+
+    @RequestMapping(value = "/view", method = RequestMethod.POST)
+    public String view(Model model, HttpServletRequest request) throws Exception {
 //		log.warn("controller.view()");
 //		log.warn(request.getParameter("seq"));
-		BoardDTO dto = service.view(Integer.parseInt((String)request.getParameter("seq")));
-		model.addAttribute("view",dto);
-		return "/board/view";
-	}
-	
-	@RequestMapping(value="/goUpdateView", method=RequestMethod.POST)
-	public String updateView(Model model, HttpServletRequest request) throws Exception{
+        BoardDTO dto = service.view(Integer.parseInt((String) request.getParameter("seq")));
+        model.addAttribute("view", dto);
+        return "/board/view";
+    }
+
+    @RequestMapping(value = "/goUpdateView", method = RequestMethod.POST)
+    public String updateView(Model model, HttpServletRequest request) throws Exception {
 //		log.warn("controller.updateView()");
 //		log.warn(request.getParameter("seq"));
 //		BoardDTO dto = service.view(6);
-		BoardDTO dto = service.view(Integer.parseInt((String)request.getParameter("seq")));
+        BoardDTO dto = service.view(Integer.parseInt((String) request.getParameter("seq")));
 //		log.warn("controller.updateView()2");
-		model.addAttribute("view", dto);
-		return "/board/update";
-	}
-	
-	@ResponseBody
-	@RequestMapping(value="/update", method=RequestMethod.POST)
-	public String update(BoardDTO dto) {
+        model.addAttribute("view", dto);
+        return "/board/update";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(BoardDTO dto) {
 //		log.warn("contorller.update()");
-		if(service.update(dto)==1) {
-			return "Y";
-		} else {
-			return "N";
-		}
-	}
-	
-	@ResponseBody
-	@RequestMapping(value="/delete", method=RequestMethod.POST)
-	public String delete(HttpServletRequest request) {
+        if (service.update(dto) == 1) {
+            return "Y";
+        } else {
+            return "N";
+        }
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(HttpServletRequest request) {
 //		log.warn("controller.delete()");
-		if(service.delete(Integer.parseInt((String)request.getParameter("seq")))==1){
-			return "Y";
-		} else {
-			return "N";
-		}
-	}
+        if (service.delete(Integer.parseInt((String) request.getParameter("seq"))) == 1) {
+            return "Y";
+        } else {
+            return "N";
+        }
+    }
+
+    @GetMapping("ex1")
+    public String ex1() {
+        return "thymeleaf/fragments/layout";
+    }
+
+    @GetMapping("ex2")
+    public String ex2() {
+        return "thymeleaf/fragments/layout2";
+    }
 }
